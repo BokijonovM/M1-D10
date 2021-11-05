5083cdba329bbf6032295bf4b002ab7dd542b409
Bokijonov Mukhsinjon <78298971+BokijonovM@users.noreply.github.com>
2021-11-05 10:12:31 +0100

    add comments and create css html js files

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+JS EXERCISES
+
+        21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
+        22) Create an object with properties such name, surname, email
+        23) Delete the email property from the previously created object
+        24) Create an array with 10 strings in it
+        25) Print in the console every string from the previous array
+        26) Create an array with 100 random numbers in it
+        27) Write a function to get the maximum and minimum values from the previously created array
+        28) Create an array of arrays, in which every array has 10 random numbers
+        29) Create a function that gets 2 arrays as parameters and returns the longest one
+        30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
+
+*/
