7e0a8d3b132deebabeb23abb01990bb541ae4b76
Bokijonov Mukhsinjon <78298971+BokijonovM@users.noreply.github.com>
2021-11-05 16:17:20 +0100

    started doing extra tasks

---
+++ extra.js
@@ -0,0 +1,15 @@
+/*
+EXTRA EXERCISES
+
+        41) Add an eventListener to show an alert when the cursor hovers a link, displaying its href property
+        42) Create a button that will hide every image on the page when clicked
+        43) Create a button that will hide or show the table on the page when clicked
+        44) Write a function for calculating the sum of every number inside all the table cells (if their content is numeric)
+        45) Delete the last letter from the heading each time the user clicks on it
+        46) Change the background color of a <td> if the user clicks on it
+        47) Add a delete button at the bottom of the table, when clicked it should delete a random <td>
+        48) Add automatically a pink border to a cell when the mouse hovers it
+        49) Write a function to create a table with 4 rows and 3 columns programmatically and add it to the bottom of the page
+        50) Write a function to remove the last table from the page
+*/
+
