50fa18437495026b12103bf10069c28c9e49eb15
Bokijonov Mukhsinjon <78298971+BokijonovM@users.noreply.github.com>
2021-11-05 14:49:51 +0100

    tasks 32-36 are completed

--- index.js
+++ index.js
@@ -15,3 +15,42 @@
 // Exercises 31
 const getDivElementsFromHTML = document.querySelectorAll("div")
 
+
+// Exercises 32
+const GetEveryTDElementFromPage = document.querySelectorAll("td")
+
+
+// Exercises 33 have a problem and isn't correct
+// i tried but cannot completed
+
+
+// Exercises 34
+const changeHeadingPage = function(){
+    let pageHeading = document.getElementById("page-heading")
+    pageHeading.innerHTML = "I am new header here"
+}
+// let output34 = changeHeadingPage()
+// console.log(output34)
+
+
+// Exercises 35
+const addNewRowToTable = function(){
+    let getTableId = document.getElementById("add-new-row")
+    let newTableRow = document.createElement("TH")
+    newTableRow.innerText = "Country"
+    getTableId.appendChild(newTableRow)
+}
+// let output35 = addNewRowToTable()
+// console.log(output35)
+
+
+// Exercises 36
+const changeStyleEachRow = function(){
+    let rowId = document.getElementById("add-new-row")
+    rowId.classList.add("test")
+}
+// et output36 = changeStyleEachRow()
+// console.log(output36)
+// Exercises 31
+// Exercises 31
+// Exercises 31
